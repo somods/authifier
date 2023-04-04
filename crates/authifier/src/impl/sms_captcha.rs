1c628e86fd16f596154c149d927ea6a9d3424bcf
Victor Tang <tanghengzhi@live.com>
2023-04-04 14:45:06 +0800

    send sms via tencent cloud

--- crates/authifier/src/impl/sms_captcha.rs
+++ crates/authifier/src/impl/sms_captcha.rs
@@ -2,7 +2,10 @@ use chrono::Duration;
 use iso8601_timestamp::Timestamp;
 use rand::Rng;
 use crate::{models::SMSCaptcha, Authifier, Success};
-use sms::aliyun::Aliyun;
+use tencentcloud_sdk_sms::{
+    client::{Client, SendSmsRequest},
+    credentials::Credential,
+};
 
 impl SMSCaptcha {
     /// Create a new SMS captcha
@@ -28,19 +31,45 @@ impl SMSCaptcha {
         authifier.database.save_sms_captcha(self).await
     }
 
-    /// Send sms via aliyun
+    /// Send sms via tencent
     pub async fn send(&self) -> Success {
-        let aliyun = Aliyun::new(
-            "LTAI5tCoAXkPkTXdHZrFBXqU",
-            "od8cOiHpDbgrWC61sgLdjq501mURk3"
+        // prepare param
+        let secret_id = "AKIDseFcmkV4ScgdR1xiBVo275sVEl9NTP8R";
+        let secret_key = "BD3e9VeLttiS1PehaHu6B9zc2MVVM4hE";
+        let sms_sdk_app_id = "1400808726";
+        let template_id = "1751340";
+        let sign_name = "MOD摩兜网";
+        let region = "ap-guangzhou";
+        let phone_number_set = vec![self.phone_number.to_owned()];
+        let template_param_set = vec![self.sms_captcha.to_owned()];
+        // build client
+        let credential = Credential::new(secret_id, secret_key, None);
+        let client = Client::new(credential, region);
+        // build request
+        let request = SendSmsRequest::new(
+            phone_number_set.clone(),
+            sms_sdk_app_id,
+            template_id,
+            sign_name,
+            template_param_set,
         );
-        let response = aliyun.send_sms(
-            &self.phone_number,
-            "阿里云短信测试",
-            "SMS_154950909",
-            &json!({"code": &self.sms_captcha}).to_string(),
-        ).await;
-        println!("{:?}", &response);
+        // send
+        let response = client.send_sms(request).await;
+        // check
+        match response {
+            Ok(res) => {
+                phone_number_set.into_iter().for_each(|phone_number| {
+                    println!(
+                        "send {}: {:?}",
+                        phone_number,
+                        res.check_is_success(phone_number.to_owned())
+                    );
+                });
+            }
+            Err(e) => {
+                println!("send error: {}", e);
+            }
+        }
 
         Ok(())
     }
